d8a3bdbc7f295c8361d362e80f109cd1348cd604
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-02 01:32:20 +0000

    friend

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -1,8 +1,17 @@
 import Menu from "@/components/Menu";
 import styles from "@/styles/Friends.module.scss";
 
+import Menu from "@/components/Menu";
+import styles from "@/styles/Friends.module.scss";
+
 export default function Friends() {
   return (<div className={styles.friends}>
     <Menu/>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <div className={styles.main}>Invite friends!</div>
+        <div className={styles.sub}>You and your friend will receive bonuses</div>
+      </div>
+    </div>
   </div>);
 }
