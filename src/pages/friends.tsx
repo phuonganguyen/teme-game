4b8d5eb7a7d6217d433e878ebb8bcddf60bbe02e
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-06 08:00:15 +0000

    friends

--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -54,55 +54,70 @@ export default function Friends({
   return (
     <Layout>
       <div className={styles.container}>
-        <div className={styles.title}>
-          <div className={styles.main}>Invite friends!</div>
-          <div className={styles.sub}>
-            You and your friend will receive bonuses
+        <div className={styles.intro}>
+          <div className={styles.title}>
+            <div className={styles.main}>Invite friends!</div>
+            <div className={styles.sub}>
+              You and your friend will receive bonuses
+            </div>
           </div>
-        </div>
-        <div className={styles.rewards}>
-          <div className={styles.item}>
-            <Image
-              src={"/images/gift1.png"}
-              width={48}
-              height={48}
-              alt="gift1"
-            />
-            <div className={styles.content}>
-              <div className={styles.title}>Invite a friend</div>
-              <div className={styles.sub}>
-                <Image src="/images/coins.png" width={20} height={20} alt="" />
-                <div>
-                  <span className={styles.number}>+5,000</span> for you and{" "}
-                  <span className={styles.number}>+2,500</span> for your friend
+          <div className={styles.rewards}>
+            <div className={styles.item}>
+              <Image
+                src={"/images/gift1.png"}
+                width={40}
+                height={40}
+                alt="gift1"
+              />
+              <div className={styles.content}>
+                <div className={styles.title}>Invite a friend</div>
+                <div className={styles.sub}>
+                  <Image
+                    src="/images/coins.png"
+                    width={20}
+                    height={20}
+                    alt=""
+                  />
+                  <div>
+                    <span className={styles.number}>+5,000</span> for you and{" "}
+                    <span className={styles.number}>+2,500</span> for your
+                    friend
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.item}>
+              <Image
+                src={"/images/gift2.png"}
+                width={40}
+                height={40}
+                alt="gift2"
+              />
+              <div className={styles.content}>
+                <div className={styles.title}>
+                  Invite a friend with Telegram Premium
+                </div>
+                <div className={styles.sub}>
+                  <Image
+                    src="/images/coins.png"
+                    width={20}
+                    height={20}
+                    alt=""
+                  />
+                  <div>
+                    <span className={styles.number}>+25,000</span> for you and{" "}
+                    <span className={styles.number}>+10,000</span> for your
+                    friend
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className={styles.item}>
-            <Image
-              src={"/images/gift2.png"}
-              width={48}
-              height={48}
-              alt="gift2"
-            />
-            <div className={styles.content}>
-              <div className={styles.title}>
-                Invite a friend with Telegram Premium
-              </div>
-              <div className={styles.sub}>
-                <Image src="/images/coins.png" width={20} height={20} alt="" />
-                <div>
-                  <span className={styles.number}>+25,000</span> for you and{" "}
-                  <span className={styles.number}>+10,000</span> for your friend
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
+
         <div className={styles["friend-list"]}>
           <div className={styles.title}>
-            {`List of your friends (${session.friends?.length || 0})`}
+            {`Total friends (${session.friends?.length || 0})`}
           </div>
           <div className={styles.list}>{renderFriends()}</div>
         </div>
