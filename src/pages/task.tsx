40c6f1bf054dc9a7b713a7501cdc96ea535bd963
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-07 16:32:35 +0000

    task

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,10 +1,16 @@
-import ComingSoon from "@/components/ComingSoon";
 import Layout from "@/components/Layout";
+import Title from "@/components/Title";
+
+import styles from "@/styles/Task.module.scss";
+import Image from "next/image";
 
 export default function Task() {
   return (
     <Layout>
-      <ComingSoon />
+      <div className={styles.container}>
+        <Title main="Earn more coins" />
+        <Image src={"/images/task/cat.png"} width={190} height={190} alt="task_cat" />
+      </div>
     </Layout>
   );
 }
