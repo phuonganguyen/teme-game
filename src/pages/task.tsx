4ab0cf80c70c2fce7716f426c1b85afa0277e202
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-09 15:17:54 +0000

    task

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,5 +1,10 @@
 import ComingSoon from "@/components/ComingSoon";
-import { IconCalendar, IconX } from "@/components/Icons";
+import {
+  IconCalendar,
+  IconTelegram,
+  IconX,
+  IconYoutube,
+} from "@/components/Icons";
 import Layout from "@/components/Layout";
 import TaskDetail from "@/components/TaskDetail";
 import TaskItem from "@/components/TaskItem";
@@ -23,19 +28,57 @@ export default function Task() {
         </div>
 
         <div className={styles.content}>
-          <div className={styles.daily}>
+          <div className={styles.tasks}>
             <div className={styles.title}>Daily tasks</div>
             <TaskItem href={"#"}>
               <IconCalendar />
               <TaskDetail name="Checkin daily" coinReward={5000} />
             </TaskItem>
-            <TaskItem href={"#"}>
+            <TaskItem
+              href={"https://x.com/Temecoinxyz/status/1810315359028957566"}
+            >
               <IconX />
               <TaskDetail name="Retweet post" coinReward={5000} />
             </TaskItem>
-            <TaskItem href={"#"}>
+            <TaskItem
+              href={"https://x.com/Temecoinxyz/status/1810315359028957566"}
+            >
               <IconX />
-              <TaskDetail name="Retweet post" coinReward={5000} />
+              <TaskDetail name="Comment post" coinReward={5000} />
+            </TaskItem>
+          </div>
+          <div className={styles.tasks}>
+            <div className={styles.tabs}>
+              <div className={`${styles.tab} ${styles.active}`}>
+                Task social
+              </div>
+              <div className={styles.tab}>Upgrade earn per hour</div>
+            </div>
+            <TaskItem href={"https://t.me/Temecoinxyz"}>
+              <IconTelegram />
+              <TaskDetail
+                name="Join our Telegram Announcement"
+                coinReward={5000}
+              />
+            </TaskItem>
+            <TaskItem href={"https://t.me/Temecoinxyz"}>
+              <IconTelegram />
+              <TaskDetail
+                name="Join our Telegram group chat"
+                coinReward={5000}
+              />
+            </TaskItem>
+            <TaskItem href={"https://x.com/Temecoinxyz"}>
+              <IconX />
+              <TaskDetail name="Follow our X account" coinReward={5000} />
+            </TaskItem>
+            <TaskItem href={"https://youtube.com/@Temecoinxyz"}>
+              <IconYoutube />
+              <TaskDetail name="Subscribe our Youtube" coinReward={5000} />
+            </TaskItem>
+            <TaskItem href={"/friends"}>
+              <IconYoutube />
+              <TaskDetail name="Invite 3 friends" coinReward={15000} />
             </TaskItem>
           </div>
         </div>
