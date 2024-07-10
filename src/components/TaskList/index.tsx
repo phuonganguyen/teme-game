9b3e85d242ad21e6fe11de566787f14f498fe94e
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-10 23:28:30 +0000

    task

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -88,6 +88,7 @@ export default function TaskList() {
 
   const handleClaim = async () => {
     if (selectedTask) {
+      window.open(selectedTask.url, "_blank");
       const response = await fetch("/api/tasks/create", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
