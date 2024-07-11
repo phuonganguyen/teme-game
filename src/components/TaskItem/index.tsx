dd4635c113675b8ac58b2b0bd6374a0b59bb2c92
Phuong Nguyen <phuong.nguyenanh06@gmail.com>
2024-07-11 14:54:22 +0000

    disabled task

--- src/components/TaskItem/index.tsx
+++ src/components/TaskItem/index.tsx
@@ -5,18 +5,24 @@ import { IconArrow, IconVerified } from "../Icons";
 type Props = {
   onClick?: () => void;
   completed?: boolean;
+  disabled?: boolean;
 };
 
 export default function TaskItem({
   children,
   onClick,
   completed = false,
+  disabled = false,
 }: PropsWithChildren<Props>) {
   const handleClick = () => {
     !completed && onClick?.();
   };
+
   return (
-    <div className={styles["task-item"]} onClick={handleClick}>
+    <div
+      className={`${styles["task-item"]} ${disabled ? styles.disabled : ""}`}
+      onClick={handleClick}
+    >
       <div className={styles.content}>{children}</div>
       {completed ? <IconVerified /> : <IconArrow />}
     </div>
