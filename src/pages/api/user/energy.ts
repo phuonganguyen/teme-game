2294f6cd9b6720a520f88d4cf4c101737e9d74c5
alvin.nguyen <alvin.nguyen@nexcel.vn>
2024-07-16 22:22:42 +0700

    energy

--- src/pages/api/user/energy.ts
+++ src/pages/api/user/energy.ts
@@ -24,7 +24,7 @@ export default async function handler(
   const energyDoc = await getDoc(energyDocRef);
   if (energyDoc.exists()) {
     const energy = energyDoc.data();
-    res.status(200).json({ energy: energy.energy, time: energy.time });
+    res.status(200).json({ energy: energy.energy, time: energy.time.toDate() });
   } else {
     res.status(200).json({ energy: 0, time: undefined });
   }
