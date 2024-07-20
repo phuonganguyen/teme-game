c9ec61396eaf775f26f069fe5da143f6711008b3
alvin.nguyen <alvin.nguyen@nexcel.vn>
2024-07-20 13:30:42 +0700

    count down

--- src/components/Energy/CountDown.tsx
+++ src/components/Energy/CountDown.tsx
@@ -1,26 +1,35 @@
-import { timeout } from "cron";
+import { parseExpression } from "cron-parser";
 import { useEffect, useState } from "react";
 
 export default function CountDown() {
-  const [remainingTime, setRemainingTime] = useState(timeout("0 */2 * * *"));
+  const expression = parseExpression("0 */2 * * *");
+  const [nextTime, setNextTime] = useState(expression.next().getTime());
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setRemainingTime(remainingTime - 1000);
+      const now = new Date().getTime();
+      const remainingTime = nextTime - now;
+      var hours = Math.floor(
+        (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor(
+        (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+      setHours(hours);
+      setMinutes(minutes);
+      setSeconds(seconds);
+
+      if (remainingTime <= 0) {
+        setNextTime(expression.next().getTime());
+      }
     }, 1000);
 
-    if (remainingTime <= 0) {
-      clearInterval(interval);
-    }
-
     return () => clearInterval(interval);
-  }, []);
-
-  var hours = Math.floor(
-    (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  }, [nextTime, expression]);
 
   return `~${hours}h ${minutes}m ${seconds}s`;
 }
